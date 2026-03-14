a8aaac39820b34671c0fedc19f3de294c55a5a84
Orlando Prieto <43296678+OrlaPrieto@users.noreply.github.com>
2026-03-13 18:46:59 -0600

    add policies for supabase

--- src/app/supabase.ts
+++ src/app/supabase.ts
@@ -3,10 +3,4 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = 'https://zjkqyqwhmqqtrknxxryk.supabase.co';
 const supabaseAnonKey = 'sb_publishable_vb_QCq6e74XxWRwtsyLEgA__qnd93_j';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  auth: {
-    persistSession: false,
-    autoRefreshToken: false,
-    detectSessionInUrl: false
-  }
-});
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
