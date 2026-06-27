bee949f8dd83e4d0dde41d33d3a58ef92048aadd
Orlando Prieto <43296678+OrlaPrieto@users.noreply.github.com>
2026-06-27 12:56:28 -0600

    config: integrate dynamic build-time environments via set-env

--- apps/frontend/src/environments/environment.prod.ts
+++ apps/frontend/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  supabaseUrl: 'https://zjkqyqwhmqqtrknxxryk.supabase.co',
-  supabaseKey: 'sb_publishable_vb_QCq6e74XxWRwtsyLEgA__qnd93_j',
-  apiUrl: 'https://clinical-nutrilev.onrender.com/api',
+  supabaseUrl: 'https://fhzoyojghnaimmczefyc.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZoem95b2pnaG5haW1tY3plZnljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5NjAzOTIsImV4cCI6MjA5NzUzNjM5Mn0.zthcBH4ExexP4TMzi2BxMX3qp48LvuXH9Z8V-2K3Sps',
+  apiUrl: 'https://TU_BACKEND_STAGING.onrender.com/api',
   menuDurationDays: 7,
   vapidPublicKey: 'BGkIPrJdZSc-BYUlBo9Ums7lIBO3Bh4rEVvFrgXP2hXxuvNH-BKRc5Nuf1XpT-xVNRYOM59lhRiZ2-BGRT9k2CQ',
   gaMeasurementId: 'G-CTM431SENR'
--- apps/frontend/src/environments/environment.ts
+++ apps/frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  supabaseUrl: 'https://zjkqyqwhmqqtrknxxryk.supabase.co',
-  supabaseKey: 'sb_publishable_vb_QCq6e74XxWRwtsyLEgA__qnd93_j',
+  supabaseUrl: 'https://fhzoyojghnaimmczefyc.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZoem95b2pnaG5haW1tY3plZnljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5NjAzOTIsImV4cCI6MjA5NzUzNjM5Mn0.zthcBH4ExexP4TMzi2BxMX3qp48LvuXH9Z8V-2K3Sps',
   apiUrl: '/api',
   menuDurationDays: 7,
   vapidPublicKey: 'BGkIPrJdZSc-BYUlBo9Ums7lIBO3Bh4rEVvFrgXP2hXxuvNH-BKRc5Nuf1XpT-xVNRYOM59lhRiZ2-BGRT9k2CQ',
